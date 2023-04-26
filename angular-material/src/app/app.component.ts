d2a4de4b60e8b60c31e6979119822faf8781cd21
Leandro Picoloto <picolotoleandro@gmail.com>
2023-04-26 10:29:17 -0300

    Testes

--- angular-material/src/app/app.component.ts
+++ angular-material/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { fromEvent, map } from 'rxjs';
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 85;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-material';
+  public isSmallScreen = false;
+  public popText = false;
+  public applyShadow = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    const scrollContainer = document.scrollingElement;
+
+    fromEvent(document, 'scroll')
+      .pipe(map(() => scrollContainer!.scrollTop))
+      .subscribe({
+        next: (value: number) => this.determineHeader(value),
+      });
+  }
+
+  determineHeader(top: number) {
+    console.log(`top ${top}`);
+    this.popText = top >= TEXT_LIMIT;
+    this.applyShadow = top >= SHADOW_LIMIT;
+  }
+
+  ngAfterContentInit(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 800px)'])
+      .subscribe((r) => (this.isSmallScreen = r.matches));
+  }
+
+  get sidenavMode() {
+    return this.isSmallScreen ? 'over' : 'side';
+  }
 }
---
+++ angular-material/src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@NgModule({
+  exports: [
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+  ],
+})
+export class MaterialModule {}
