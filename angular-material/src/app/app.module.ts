56d5f87937ab4a64846d60bfb513e4043e49f483
Leandro Picoloto <picolotoleandro@gmail.com>
2023-04-26 10:48:57 -0300

    Montando fake storybook

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { ButtonIconComponent } from './button-icon/button-icon.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ButtonIconComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
